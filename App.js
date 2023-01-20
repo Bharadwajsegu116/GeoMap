b37184a76b63ee4639071af3799090e5f033eb4f
Segu Sai Bharadwaj <89660288+Bharadwajsegu116@users.noreply.github.com>
2023-01-20 16:21:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Geomap from './geocomponents/Geomap';
+import Geocontrol from './geocomponents/Geocontrol';
+function App() {
+  <><Geomap /><Geocontrol /></>
+}
+
+export default App;
